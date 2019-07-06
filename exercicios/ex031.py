eacb21db5f621d95628512657af838cf20cbfc0c
Ricardo <rickmaru@msn.com>
2019-07-06 18:02:43 -0300

    teste

---
+++ exercicios/ex031.py
@@ -0,0 +1,7 @@
+distancia = int(input('digite a distancia'))
+
+if(distancia > 200 ):
+    print('Valor da passagem é {}'.format(float(distancia * 0.45)))
+else:
+    print('valor da passagem é {}'.format(float(distancia * 0.5)))
+
