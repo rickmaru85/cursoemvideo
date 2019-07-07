362db01ef6208df5f233c6c13eeb534e50f82c94
Ricardo <rickmaru@msn.com>
2019-07-07 12:56:24 -0300

    teste

--- exercicios/ex062.py
+++ exercicios/ex062.py
@@ -1,8 +1,17 @@
 termo = int(input('digite o primeiro termo:'))
 razao = int(input('Digite a razao'))
 
+contador = int(10)
+flag = str('')
 
-for c in range (0,10,1,):
+while flag == '':
     print(termo)
-    termo += razao
-print(termo)
+    termo = termo + razao
+    contador -= 1
+    if contador == 0:
+        contador = int(input('Deseja exibir mais quantos numero?'))
+        print(contador)
+        if(contador == 0):
+            flag = 'x'
+
+
