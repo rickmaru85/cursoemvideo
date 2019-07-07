1be7ed0429eae13aeff0076fd65b941e8eb6dac8
Ricardo <rickmaru@msn.com>
2019-07-07 12:56:13 -0300

    teste

--- exercicios/ex061.py
+++ exercicios/ex061.py
@@ -4,14 +4,8 @@ razao = int(input('Digite a razao'))
 contador = int(10)
 flag = str('')
 
-while flag == '':
+while contador == 0:
     print(termo)
     termo = termo + razao
     contador -= 1
-    if contador == 0:
-        contador = int(input('Deseja exibir mais quantos numero?'))
-        print(contador)
-        if(contador == 0):
-            flag = 'x'
-
 
